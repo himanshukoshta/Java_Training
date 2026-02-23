85cbff6ce129c05177f6f7e666d90327bcdc0d81
himanshukoshta <koshtahimanshu89@gmail.com>
2026-02-23 21:40:33 +0530

    Sum of two element whose sum is equal to the given target value

---
+++ sum_max.java
@@ -0,0 +1,20 @@
+package training;
+
+public class sum_max {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,0};
+        int k=3;
+
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++){
+               if(arr[i]+arr[j]==k)
+               {
+                System.out.println(i +""+ j);
+                break;
+               }
+            }
+        }
+       
+    }
+}
