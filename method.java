b0c106f242a2c4ca43df4662602a4502d613f4cd
himanshukoshta <koshtahimanshu89@gmail.com>
2026-02-25 22:35:07 +0530

    Methods

---
+++ method.java
@@ -0,0 +1,12 @@
+package training;
+
+
+public class method {
+    void sum(){
+    System.out.println(10+2);
+}
+    public static void main(String[] args) {
+        method m=new method();
+        m.sum();
+    }
+}
