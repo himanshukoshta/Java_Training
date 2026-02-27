57b9546941a28bad5c6cb6bd18986727f4515315
himanshukoshta <koshtahimanshu89@gmail.com>
2026-02-27 23:37:27 +0530

    String

---
+++ string1.java
@@ -0,0 +1,11 @@
+package training;
+
+public class string1 {
+    public static void main(String[] args) {
+        String s="hello";
+        for(int i=0;i<s.length();i++)
+        {
+            System.out.println(s.charAt(i));
+        }
+    }
+}
